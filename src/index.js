d3df55601a3a06926766cb139b7186b26f359483
Rene Morgado <renemorgado33@gmail.com>
2020-10-09 00:01:51 -0500

    API en heroku

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ app.set('port', process.env.PORT || '8000');
 app.use('/static', express.static(__dirname + 'public'));
 require('./database');
 app.use('/api', apiURI);
+app.get('/', (req, res) => {
+    res.send({
+        "Your-location": "Rene's API",
+        "True-website": "webURL",
+        "Please": "Visit me ;)"
+    })
+})
 try {
     app.listen(app.get('port'), () => {
         console.log('Server running on port: ' + app.get('port'));
