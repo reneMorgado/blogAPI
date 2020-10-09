6a89701bbe58767bb008acd0d1c35052c39bef83
Rene Morgado <renemorgado33@gmail.com>
2020-10-08 23:05:45 -0500

    API lista

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const apiURI = require('./routes/home')
 const app = express();
 
 app.set('port', '80');
 app.use('/static', express.static(__dirname + 'public'));
-require('./database')
-
+require('./database');
+app.use('/api', apiURI);
 try {
     app.listen(app.get('port'), () => {
         console.log('Server running on port: ' + app.get('port'));
